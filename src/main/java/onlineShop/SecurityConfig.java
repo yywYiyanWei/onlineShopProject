c2b4fce35e272ab09598434425f4e0a3c3430403
YiyanWei <kalosoleo@gmail.com>
2021-01-15 22:25:20 -0500

    Finished customer-related functionity

---
+++ src/main/java/onlineShop/SecurityConfig.java
@@ -0,0 +1,58 @@
+package onlineShop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // currently not have logic
+                .formLogin().loginPage("/login"); // using my own login page
+        http
+                .authorizeRequests()
+                .antMatchers("/cart/**").hasAuthority("ROLE_USER")
+                .antMatchers("/get*/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                .antMatchers("/admin*/**").hasAuthority("ROLE_ADMIN")
+                .anyRequest().permitAll(); // for any other requests, no authority required
+        http
+                .logout()
+                .logoutUrl("/logout");
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // test admit user
+        auth
+                .inMemoryAuthentication().withUser("admin@gmail.com").password("12345678").authorities("ROLE_ADMIN");
+
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT emailId, password, enabled FROM users WHERE emailId=?")
+                .authoritiesByUsernameQuery("SELECT emailId, authorities FROM authorities WHERE emailId=?");
+
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+
+}
